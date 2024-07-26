a3f3cdaea0ab4db2ebc4174a3628f9d6069c0e45
Eyo Okon Eyo <eyo.eyo@elastic.co>
2024-07-26 18:08:54 +0200

    slight adjustments rendered items count

--- x-pack/plugins/spaces/public/management/view_space/view_space_roles.tsx
+++ x-pack/plugins/spaces/public/management/view_space/view_space_roles.tsx
@@ -56,11 +56,7 @@ interface Props {
 export const filterRolesAssignedToSpace = (roles: Role[], space: Space) => {
   return roles.filter((role) =>
     role.kibana.reduce((acc, cur) => {
-      return (
-        (cur.spaces.includes(space.name) || cur.spaces.includes('*')) &&
-        Boolean(cur.base.length) &&
-        acc
-      );
+      return (cur.spaces.includes(space.name) || cur.spaces.includes('*')) && acc;
     }, true)
   );
 };
